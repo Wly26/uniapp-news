8973d2b6c34c1c33486fe0806a5f98d71a1ca3f6
Wly26 <1402404992@qq.com>
2022-07-31 18:12:40 +0800

    第五章

--- components/list/list-item.vue
+++ components/list/list-item.vue
@@ -1,8 +1,7 @@
 <template>
 	<list-scroll class="list-scroll">
 		<list-card mode="base" :item="item" v-for="item in list" :key="item._id"></list-card>
-<!-- 		<list-card mode="column"></list-card>
-		<list-card mode="image"></list-card> -->
+		<uni-load-more iconType="snow" status="noMore"></uni-load-more>
 	</list-scroll>
 </template>
 
